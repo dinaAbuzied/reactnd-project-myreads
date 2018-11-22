9464d35ad9f898e81bf70eebfc7674ed48a0df65
Dina Abuzied <dina.abuzied@gmail.com>
2018-11-22 05:20:47 +0200

    added BooksAPI to show books on shelves and added searching functionalities

--- src/App.js
+++ src/App.js
@@ -1,32 +1,79 @@
 import React, {Component} from 'react'
 import { Route } from 'react-router-dom'
-// import * as BooksAPI from './BooksAPI'
+import * as BooksAPI from './BooksAPI'
 import BookShelves from './BookShelves'
 import BookSearch from './BookSearch'
 import './App.css'
 
 class BooksApp extends Component {
   state = {
-    
+    books: [],
+    searchReasult: [],
+    query: ""
+  }
+
+  componentDidMount = () => {
+    this.getAllBooks();
+  }
+
+  getAllBooks = () => {
+    BooksAPI.getAll().then((books) => {
+      this.setState({books});
+    })
+  }
+
+  updateBook = (book, shelf) => {
+    BooksAPI.update(book, shelf).then(() => {
+      console.log("updated");
+      this.getAllBooks();
+    })
+  }
+
+  searchBooks = (query) => {
+    if(query === ""){
+      this.setState({searchReasult: [], query: ""});
+    }
+    else {
+      this.setState({query});
+      BooksAPI.search(query).then((searchReasult) => {
+        if (Array.isArray(searchReasult) && this.state.query !== ""){
+          let books = this.state.books;
+          for (let index = 0; index < searchReasult.length; index++) {
+            let book = searchReasult[index];
+            let filteredBooks = books.filter(savedBook => book.id === savedBook.id);
+            if(filteredBooks.length > 0)
+            book.shelf = filteredBooks[0].shelf;
+          }
+          console.log(searchReasult);
+          this.setState({searchReasult});
+        }
+        else {
+          this.setState({searchReasult: []});
+        }
+      })
+    }
   }
 
   render() {
     return (
       <div className="app">
         <Route exact path="/" render={() => (
-          <BookShelves
-            onNavigate= {
+          <BookShelves books = {this.state.books}
+            onNavigate = {
               () => {
                 this.setState({ showSearchPage: true })
               }}
+            updateBook = {this.updateBook}
           />
         )}/>
         <Route exact path="/search" render={() => (
-          <BookSearch 
+          <BookSearch updateBook = {this.updateBook}
             onNavigate= {
               () => {
                 this.setState({ showSearchPage: false })
               }}
+            searchBooks = {this.searchBooks} 
+            searchReasult = {this.state.searchReasult}
           />
         )}/>
       </div>
--- src/BookSearch.js
+++ src/BookSearch.js
@@ -2,26 +2,61 @@ import React, { Component } from 'react'
 import { Link } from 'react-router-dom'
 
 class BookSearch extends Component {
+    createBook = () => {
+        let books = [];
+        
+        for (let index = 0; index < this.props.searchReasult.length; index++) {
+          let book = this.props.searchReasult[index];
+          books.push(
+            <li key={book.id}>
+                <div className="book">
+                  <div className="book-top">
+                    <div className="book-cover" style={{ width: 128, height: 193, backgroundImage: `url(${book.imageLinks ? book.imageLinks.smallThumbnail : ''})` }}></div>
+                      <div className="book-shelf-changer">
+                        <select value={book.shelf ? book.shelf : 'none'} onChange={(event) => {
+                          book.shelf = event.target.value;
+                          this.props.updateBook(book, event.target.value);
+                        }}>
+                          <option value="move" disabled>Move to...</option>
+                          <option value="currentlyReading">Currently Reading</option>
+                          <option value="wantToRead">Want to Read</option>
+                          <option value="read">Read</option>
+                          <option value="none">None</option>
+                        </select>
+                      </div>
+                    </div>
+                  <div className="book-title">{book.title}</div>
+                  {
+                    (book.authors) ? (
+                      book.authors.map((author, index) => (
+                        <div key={"author" + index.toString()} className="book-authors">{author}</div>
+                      ))
+                    ) : (
+                        <div key={"author" + index.toString()} className="book-authors">unknown</div>
+                    )}
+              </div>
+            </li>
+          )
+        }
+        return books;
+      }
     render(){
         return(
             <div className="search-books">
             <div className="search-books-bar">
               <Link to="/" className="close-search" onClick={this.props.onNavigate}>Close</Link>
               <div className="search-books-input-wrapper">
-                {/*
-                  NOTES: The search from BooksAPI is limited to a particular set of search terms.
-                  You can find these search terms here:
-                  https://github.com/udacity/reactnd-project-myreads-starter/blob/master/SEARCH_TERMS.md
-
-                  However, remember that the BooksAPI.search method DOES search by title or author. So, don't worry if
-                  you don't find a specific author or title. Every search is limited by search terms.
-                */}
-                <input type="text" placeholder="Search by title or author"/>
+                <input type="text" placeholder="Search by title or author" onChange= {(event) => {
+                    console.log(event.target.value);
+                    this.props.searchBooks(event.target.value)
+                }}/>
 
               </div>
             </div>
             <div className="search-books-results">
-              <ol className="books-grid"></ol>
+              <ol className="books-grid">
+                  {this.createBook()}
+              </ol>
             </div>
           </div>
         );
